ca6bc8a3983e326fbbf667e39432e7719d716d53
roydensynderen <roydensynderen@gmail.com>
2021-12-06 09:45:19 -0800

    import fn

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,16 @@
+import { useEffect } from 'react'
+import play from '../playground'
+
 export default function Home() {
 
-  const message: string = "Hello World"
+  useEffect(() => {
+    play();
+  }, [])
+
 
   return (
     <div>
-      { message }
+      Hello World
     </div>
   )
 }
