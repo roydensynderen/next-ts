58ee69e5d72b07819d48aff2785a3d3f209e1905
roydensynderen <roydensynderen@gmail.com>
2021-12-08 14:11:44 -0800

    add typescript e.gs

--- playground.ts
+++ playground.ts
@@ -1,3 +1,39 @@
+interface Person {
+  name: string,
+  age: number
+}
+
+// type Person = {
+//   name: string,
+//   age: number
+// }
+
 export default function play() {
+  const name = 'R'
+  const age = 28
+  const person = {
+    name: 'D',
+    age: 22
+  }
+
   console.log('hello world')
+
+  function logPersonInfo(name: string, age: number) {
+
+    const info = `Name: ${name}, Age: ${age}`
+    console.log(info)
+
+    return info
+  }
+
+  function logPersonInfo2(person: Person) {
+
+    const info = `Name: ${person.name}, Age: ${person.age}`
+    console.log(info)
+
+    return info
+  }
+
+  logPersonInfo(name, age)
+  logPersonInfo2(person)
 }
